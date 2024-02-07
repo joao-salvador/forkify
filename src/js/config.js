ae891e2281bd2fd439a14166fb3f0edcaca193ef
= <=>
2024-02-07 15:55:27 -0300

    Inicial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a207351b-3682-40a6-bbf7-d329c580b1b3';
+export const MODAL_CLOSE_SEC = 2.5;
